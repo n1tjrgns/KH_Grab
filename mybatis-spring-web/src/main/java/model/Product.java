76211c6234807ce8993cdbc1073942f9a2682367
jin9 <user1@DESKTOP-BIBKLL9>
2017-12-13 11:18:30 +0900

    product 모델 추가

---
+++ mybatis-spring-web/src/main/java/model/Product.java
@@ -0,0 +1,86 @@
+package model;
+
+import java.util.Date;
+
+public class Product {
+	private String prodName;
+	private String prodCategory;
+	private int prodStock;
+	private String prodContent;
+	private int prodPrice;
+	private String prodPic;
+	private String cEmail;
+
+	public Product(String prodName, String prodCategory, int prodStock, String prodContent, int prodPrice,
+			String prodPic, String cEmail) {
+		this.prodName = prodName;
+		this.prodCategory = prodCategory;
+		this.prodStock = prodStock;
+		this.prodContent = prodContent;
+		this.prodPrice = prodPrice;
+		this.prodPrice = prodPrice;
+		this.cEmail = cEmail;
+	}
+
+	public Product() {
+
+	}
+
+	public String getProdName() {
+		return prodName;
+	}
+
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+
+	public String getProdCategory() {
+		return prodCategory;
+	}
+
+	public void setProdCategory(String prodCategory) {
+		this.prodCategory = prodCategory;
+	}
+
+	public int getProdStock() {
+		return prodStock;
+	}
+
+	public void setProdStock(int prodStock) {
+		this.prodStock = prodStock;
+	}
+
+	public String getProdContent() {
+		return prodContent;
+	}
+
+	public void setProdContent(String prodContent) {
+		this.prodContent = prodContent;
+	}
+
+	public int getProdPrice() {
+		return prodPrice;
+	}
+
+	public void setProdPrice(int prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+
+	public String getProdPic() {
+		return prodPic;
+	}
+
+	public void setProdPic(String prodPic) {
+		this.prodPic = prodPic;
+	}
+
+	public String getcEmail() {
+		return cEmail;
+	}
+
+	public void setcEmail(String cEmail) {
+		this.cEmail = cEmail;
+	}
+
+	
+}
